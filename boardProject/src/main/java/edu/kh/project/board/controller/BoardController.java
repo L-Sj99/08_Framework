9efb4b2b38c9df3a5458305d2de1bcb375befde3
L-sj99 <yms6897@naver.com>
2024-10-11 16:18:43 +0900

    20241011_게시글 댓글 관련(작성, 수정, 삭제)

--- boardProject/src/main/java/edu/kh/project/board/controller/BoardController.java
+++ boardProject/src/main/java/edu/kh/project/board/controller/BoardController.java
@@ -22,6 +22,7 @@ import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import edu.kh.project.board.dto.Board;
+import edu.kh.project.board.dto.Comment;
 import edu.kh.project.board.dto.Pagination;
 import edu.kh.project.board.service.BoardService;
 import edu.kh.project.member.dto.Member;
@@ -217,5 +218,31 @@ public class BoardController {
 		return service.boardLike(boardNo, memberNo);
 	}
 	
+	/** 댓글 목록 조회(비동기)
+	 * @param boardNo : 게시글 번호(쿼리스트링 전달 받음)
+	 * @param model : forward 대상에게 데이터를 전달하는 객체
+	 * @return
+	 */
+	@GetMapping("commentList")
+	public String selectCommentList(@RequestParam("boardNo") int boardNo, Model model) {
+		List<Comment> commentList = service.selectCommentList(boardNo);
+		/* 평균 비동기 통신(AJAX) 방법 : 요청 -> 응답(데이터)
+		 * * forward : 요청 위임(요청에 대한 응답 화면(HTML) 생성을 템플릿 엔진(jsp, Thymeleaf 등)이 대신 수행)
+		 * - forward는 동기식이 아닌 템플릿 엔진을 이용해서 html 코드를 쉽게 생성
+		 * 
+		 * @ResponsBody : 컨트롤러에서 반환되는 값을 응답 본문에 그대로 반환 시켜주는 역할
+		 * - 템플릿 엔진(Thymeleaf)를 이용해서 HTML코드를 만들어서 반환하는 것이 아닌 데이터를 있는 그대로 반환
+		 */
+		
+		// Board 객체 생성
+		Board board = Board.builder().commentList(commentList).build();
+		
+		// "board"라는 key 값으로 생성한 Board 객체를 forward 대상인 comment.html로 전달
+		model.addAttribute("board", board);
+		
+		// comment.html 중 comment-list 조각(fragment)작성된 thymeleaf 코드를 해석해서 완전한 HTML 코드로 변환 후
+		// 요청한 곳으로 응답(fetch() API 코드로 html 코드가 반환)
+		return "board/comment :: comment-list";
+	}
 	
 }
